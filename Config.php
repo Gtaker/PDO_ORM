abff704d228ab41374dec2f061b042fafd70959a
Gtacer <459472218@qq.com>
2017-10-08 15:11:15 +0800

    remove config file

---
+++ Config.php
@@ -0,0 +1,10 @@
+<?php
+
+class Config
+{
+    const HOST = 'localhost';
+    const USERNAME = 'root';
+    const PSD = 'root';
+    const DBNAME = 'weekly';
+    const SORT = 3306;
+}
--- ORM.php
+++ ORM.php
@@ -1,13 +1,6 @@
 <?php
 
-class Config
-{
-    const HOST = 'localhost';
-    const USERNAME = 'root';
-    const PSD = 'root';
-    const DBNAME = 'weekly';
-    const SORT = 3306;
-}
+require_once 'Config.php';
 
 
 class ORM
